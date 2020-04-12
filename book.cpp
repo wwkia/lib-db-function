acb0682e8c51776541e918c84ac61f25136949aa
wwkia <63380155+wwkia@users.noreply.github.com>
2020-04-12 23:53:28 +0000

    Update book.cpp

--- book.cpp
+++ book.cpp
@@ -1,3 +1,5 @@
+//TESTED
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,19 +10,19 @@ using namespace std;
 
 //constructor for the Book class
 Book::Book()
-:title(""), author(""), date(0), type(0), isbn({0,0}), pages(0) {}
+:title(""), author(""), date(0), type(0), isbn{0,0}, pages(0) {}
 //returns the integer book type as the string which the int represents
-string Book::type_to_string (int n) {
+string Book::type_to_string (int n) const {
 	if (type==1) 
-		return << "hard cover";
+		return "hard cover";
 	if (type==2)
-		return << "soft cover";
+		return "soft cover";
 	if (type==3)
-		return << "digital";
+		return "digital";
 }
 //operator overload enables printing of a book
 ostream& operator<<(ostream& out, const Book& entry) {
-	str_type=Book.type_to_string(entry.type);
+	string str_type=entry.type_to_string(entry.type);
 	return out << entry.title << "*" << entry.author << "*"<< entry.date << "-"
 	           << str_type << "*" << entry.isbn[0] << "*" << entry.isbn[1] << "*" << entry.pages <<'\n';
 }
@@ -37,13 +39,9 @@ void Book::print() {
 	cout << "\n" << bold << title << def_txt << "*";
 	cout << author << "*";
 	cout << date << "*";
-	cout isbn[0] << "*" << isbn[1]<< "*";
+	cout << isbn[0] << "*" << isbn[1]<< "*";
 	cout << pages;
 
-}
-bool Book::valid_year(int date) {
-	current_year = tm_year + 1900;
-	return (date>1000 && date <= current_year);
 }
 void Book::set_title() {
 	string ttl;
@@ -58,6 +56,13 @@ void Book::set_author() {
 	while(!(getline(cin, aut))) 
 		ui_error("author");
 	author=aut;
+}
+//checks if the year entered is between 1000 and now
+bool Book::valid_year(int date) {
+	time_t current_time=time(0);
+	tm*ltm=localtime(&current_time);
+	int current_year=ltm->tm_year + 1900;
+	return (date>1000 && date <= current_year);
 }
 void Book::set_date() {
 	int dt;
@@ -76,7 +81,7 @@ bool Book::is_ten_digit(double entry) {
 	return (entry < 10000000000 && entry > 999999999);
 }
 bool Book::valid_isbn (double entry) {
-	return ((is_ten_digit) || (entry < 10000000000000 && entry > 999999999999));
+	return ((is_ten_digit(entry)) || (entry < 10000000000000 && entry > 999999999999));
 }
 void Book::set_isbn() {
 	double new_isbn[2]={0,0};
@@ -91,16 +96,16 @@ void Book::set_isbn() {
 		cout << "ISBN #" << i << ": ";
 		while (!(cin>>isbn_entry)|| !(valid_isbn(isbn_entry)))
 			ui_error("ISBN number");
-		if (is_ten_digit(isnbn_entry))
+		if (is_ten_digit(isbn_entry))
 			isbn[0]=isbn_entry;
 		else 
 			isbn[1]=isbn_entry;
 	}
 }
-void set_pages() {
+void Book::set_pages() {
 	int pgs;
 	cout << "\n# of pages: ";
-	while (!(cin>>pages)) 
+	while (!(cin>>pgs)) 
 		ui_error("number of pages");
 	pages=pgs;
 }
