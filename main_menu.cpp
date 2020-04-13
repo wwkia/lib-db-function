9cfe66758792cc022a08b296153136c9fe596f97
wwkia <63380155+wwkia@users.noreply.github.com>
2020-04-13 05:05:43 +0000

    Update main_menu.cpp

--- main_menu.cpp
+++ main_menu.cpp
@@ -13,42 +13,46 @@ Main_menu::Main_menu(string f, int n)
 
 //switch funtion for navigating the main menu
 void Main_menu::switch_main(int choice, Database db) {
+	int state;
 	switch(choice) {
    		case 1:
-   			int state=db.add();
+   			//state=db.add();
    			//opens add function if user chose to add again
    			if (state==1)
    				switch_main(1,db);
    			break;
    		case 2: 
-   			int state=db.del();
+   			state=db.del();
    			//open delete function if user chose to delete again
    			if (state==1)
    				switch_main(2,db);
    			break;
    		case 3:
-   			int state=db.update();
+   			state=db.update();
    			if (state==1)
    				switch_main(3,db);
    			break;
    		case 4:
-   			int state=db.search(0);
+   			//int state=db.search(0);
    			break;
    		case 5:
-   			int state=db.save();
+   			state=db.save();
    			//opens save function if error occurred and user chose to try again
    			if (state==1)
-   				switch_main(5);
+   				switch_main(5,db);
    			break;
    		case 6:
-   			int state=db.load();
+   			state=db.load();
    			//opens load function if error occurred and the user chose to try gain
    			if (state==1)
-   				switch_main(6);
+   				switch_main(6,db);
    			break;  		
    		case 7:
    			cout << clrscrn << "Goodbye!";
    			break;
+   		default:
+   			break;
    		}
 }
+
 #endif
