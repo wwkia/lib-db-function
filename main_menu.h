3b5646c8c08b5b13fc2b78ad02b5ea5098febd38
wwkia <63380155+wwkia@users.noreply.github.com>
2020-04-13 05:06:20 +0000

    Update main_menu.h

--- main_menu.h
+++ main_menu.h
@@ -7,6 +7,6 @@ using namespace std;
 class Main_menu : public Menu {
 	public:
 		Main_menu(string f, int n);
-		void switch_main(int choice);
+		void switch_main(int choice, Database db);
 };
 #endif
